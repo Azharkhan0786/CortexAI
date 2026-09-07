19670bb89ca0ea4025c327ae3b52d4b8535d6bbe
Azhar Khan <azharkhan844716@gmail.com>
2026-09-07 16:29:28 +0000

    added f firebase

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+ return (
+  <div className='w-full h-screen bg-black flex items-center justify-center'>
+    <button className='w-50 h-24 bg-white text-black'>continue with google</button>
+  </div>
+ )
+}
+
+export default App
