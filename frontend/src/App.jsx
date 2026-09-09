08c8e1418862f55d5aea7b4c8c28c26dd46fcf90
Azhar Khan <azharkhan844716@gmail.com>
2026-09-09 09:35:09 +0000

    feat:added login function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { signInWithPopup } from 'firebase/auth'
 import { auth, googleProvider } from '../utils/firebase'
+import api from '../utils/axios'
 
 function App() {
 
+  const handleLogin=async (token)=>{
+    try{
+      const {data}=await api.post("/auth/login",{token})
+      console.log(data)
+    }
+    catch(error){
+      console.error("Error logging in:", error)
+    }
+  }
+
   const googleLogin=async()=>{
     const data=await signInWithPopup(auth,googleProvider)
+    const token=await data.user.getIdToken()
+    console.log(token)
+    await handleLogin(token)
     console.log(data)
 
   } 
